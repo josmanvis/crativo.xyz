49e6cd25d9cf7288cde5c56686e288227a93208d
Taco Pants <tacopants@icloud.com>
2026-01-25 19:03:40 -0500

    Fix grid: 2 columns on all screen sizes, 3 on lg

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -8,7 +8,7 @@ interface ProjectGridProps {
 export default function ProjectGrid({ projects }: ProjectGridProps) {
   return (
     <div className="max-w-6xl mx-auto px-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -8,7 +8,7 @@ interface BlogGridProps {
 export default function BlogGrid({ posts }: BlogGridProps) {
   return (
     <div className="max-w-6xl mx-auto px-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
         {posts.map((post) => (
           <BlogCard key={post.slug} post={post} />
         ))}
